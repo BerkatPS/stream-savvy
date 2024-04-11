a3ed3c4f40ceda63bfa92991771eafed7637e0d3
Berkat Saragih <berkat.sragih@gmail.com>
2024-04-11 17:24:34 +0700

    Fixing Movies Ui View

--- Streaming-Film/Core/Movies/MoviesUiView.swift
+++ Streaming-Film/Core/Movies/MoviesUiView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 import AVKit
 
 struct MoviesUiView: View {
+    @State private var showFullText = false
+    let synopsisLongText = "Ku Kejar Mimpi (Chasing My Dreams) is a film that tells the story of a young man named Alex who dreams of becoming a famous musician. Alex grows up in a tough environment full of challenges, but he possesses remarkable musical talent and a strong determination to pursue his dreams. Despite facing various obstacles and setbacks, including rejection from his family and financial difficulties, Alex never gives up on his musical aspirations."
     
     var body: some View {
         NavigationView{
@@ -16,6 +18,7 @@ struct MoviesUiView: View {
                 Color.black
                 
                 ScrollView(.vertical, showsIndicators: false){
+                    //
                     VStack{
                         Image("Thumbnail-foryou")
                             .resizable()
@@ -160,7 +163,7 @@ struct MoviesUiView: View {
                                                     .foregroundColor(.white)
                                                     .frame(width: 40, height: 20)
                                                     .padding(20)
-                                                    .background(Color.gray.opacity(0.4))
+                                                    .background(Color.yellow.opacity(0.9))
                                                     .foregroundColor(.white)
                                                     .clipShape(Circle())
                                             }
@@ -196,18 +199,90 @@ struct MoviesUiView: View {
                         
                     }
                     
+                    // Synopsis
                     VStack(alignment: .leading, spacing: 30){
                         Text("Synopsis")
                             .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
                             .fontWeight(.bold)
                             
                         
-                        Text("Ku Kejar Mimpi (Chasing My Dreams) is a film that tells the story of a young man named Alex who dreams of becoming a famous musician. Alex grows up in a tough environment full of challenges, but he possesses remarkable musical talent and a strong determination to pursue his dreams. Despite facing various obstacles and setbacks, including rejection from his family and financial difficulties, Alex never gives up on his musical aspirations.")
+                        Text(showFullText ? synopsisLongText : "\(synopsisLongText.prefix(100))..")
+                            .foregroundStyle(Color.white.opacity(0.6))
+                            .lineLimit(nil)
+                            .fixedSize(horizontal: false, vertical: true)
+                        
+                        if !showFullText {
+                            Button(action: {
+                                showFullText.toggle()
+                            }) {
+                                Text("Read More")
+                                    .foregroundStyle(Color("primaryColor"))
+                            }
+                        }
                     }
                     .padding()
                     .padding(.vertical, 20)
                     .background(Color.gray.opacity(0.2))
                     .foregroundColor(.white)
+                    
+                    //Director
+                    
+                    VStack(alignment: .leading, spacing: 20) {
+                        Text("Top Cast")
+                            .font(.title)
+                            .fontWeight(.bold)
+                        
+                        ScrollView(.horizontal, showsIndicators: false){
+                            HStack{
+                                VStack{
+                                    Image("Thumbnail-foryou")
+                                        .resizable()
+                                        .clipShape(Circle())
+                                        .frame(width: 60, height: 60)
+                                    
+                                    Text("Aisyah Aqilah")
+                                        .fontWeight(.bold)
+                                        .font(.footnote)
+                                }
+                                VStack{
+                                    Image("Thumbnail-foryou")
+                                        .resizable()
+                                        .clipShape(Circle())
+                                        .frame(width: 60, height: 60)
+                                    
+                                    Text("Boah Sartika")
+                                        .fontWeight(.bold)
+                                        .font(.footnote)
+                                }
+                                VStack{
+                                    Image("Thumbnail-foryou")
+                                        .resizable()
+                                        .clipShape(Circle())
+                                        .frame(width: 60, height: 60)
+                                    
+                                    Text("Novia Bachmid")
+                                        .fontWeight(.bold)
+                                        .font(.footnote)
+                                }
+                                VStack{
+                                    Image("Thumbnail-foryou")
+                                        .resizable()
+                                        .clipShape(Circle())
+                                        .frame(width: 60, height: 60)
+                                    
+                                    Text("Azela Putri")
+                                        .fontWeight(.bold)
+                                        .font(.footnote)
+                                }
+                            }
+                        }
+                    }
+                    .padding()
+                    .padding(.vertical, 20)
+                    .background(Color.gray.opacity(0.2))
+                    .foregroundColor(.white)
+
+                    
                     
                     // Result Comment Users
                     VStack(alignment: .leading){
